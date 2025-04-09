e6bb6b0be8dafdca1c34d91047d12772ae990cb1
Szilard Szurap <szurap.szilard@gmail.com>
2025-04-09 21:11:45 +0200

    Update BackendApplication.java

--- backend/src/main/java/com/nepkey/backend/BackendApplication.java
+++ backend/src/main/java/com/nepkey/backend/BackendApplication.java
@@ -9,5 +9,13 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+	private int egy = 1;
 
+    public int getEgy() {
+        return egy;
+    }
+
+    public void setEgy(int egy) {
+        this.egy = egy;
+    }
 }
