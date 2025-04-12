4d3f2bbf92e7045512ab3db9474de1b128b27b96
Szilard Szurap <szurap.szilard@gmail.com>
2025-04-13 00:11:45 +0200

    calendar_beta

--- frontend/src/ui/App.tsx
+++ frontend/src/ui/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Calendar from './Calendar';
+import Home from '../Home'; // Főoldal, ha létezik
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calendar" element={<div><h1>Naptár</h1><Calendar /></div>} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
